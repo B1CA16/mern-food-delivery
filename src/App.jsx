dec15fc3711ccad2c1495e50567f038239bbbb6e
Francisco Ferreira <121575578+B1CA16@users.noreply.github.com>
2024-07-07 13:42:37 +0100

    Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import { Route, Routes } from 'react-router'
 
 const App = () => {
   return (
     <div className='w-10/12 m-auto'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
