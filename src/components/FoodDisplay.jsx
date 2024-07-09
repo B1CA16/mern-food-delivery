057e404a399b2a7b53e350d91790b8884507b5d3
Francisco Ferreira <121575578+B1CA16@users.noreply.github.com>
2024-07-09 14:16:16 +0100

    Food List

---
+++ src/components/FoodDisplay.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { StoreContext } from '../context/StoreContext'
+import FoodItem from './FoodItem'
+
+const FoodDisplay = ({category}) => {
+  const {food_list} = useContext(StoreContext)
+
+  return (
+    <div className='mt-8'>
+      <h1 className='text-3xl font-bold dark:text-neutral-200'>Top dishes near you</h1>
+      <div style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(240px, 1fr))' }} className='grid mt-8 gap-8 gap-y-12'>
+        {food_list.map((item, index) => {
+          return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
---
+++ src/components/FoodItem.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { StoreContext } from '../context/StoreContext'
+
+const FoodItem = ({id, name, description, price, image}) => {
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
+
+  return (
+    <div className='w-full m-auto rounded-2xl shadow-md dark:shadow-neutral-950 animate-fade'>
+      <div className='relative'>
+        <img style={{ borderRadius: '15px 15px 0px 0px'}} className='w-full' src={image} alt={name} />
+        {!cartItems[id]
+          ? <img className='w-9 absolute bottom-4 right-4 rounded-full cursor-pointer hover:opacity-85' onClick={() => addToCart(id)} src={assets.add_icon_white} alt='add' /> 
+          : <div className='absolute bottom-4 right-4 flex items-center gap-2 p-1 rounded-full bg-white'>
+              <img className='w-7 cursor-pointer hover:opacity-85' onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt='remove' /> 
+              <p>{cartItems[id]}</p>
+              <img className='w-7 cursor-pointer hover:opacity-85' onClick={() => addToCart(id)} src={assets.add_icon_green} alt="add" />
+            </div>
+        }
+      </div>
+      <div className='p-5'>
+        <div className='flex justify-between items-center mb-2'>
+          <p className='text-xl font-semibold dark:text-neutral-200'>{name}</p>
+          <img className='w-18' src={assets.rating_starts} alt="star" />
+        </div>
+        <p className='dark:text-neutral-200'>{description}</p>
+        <p className='text-orange-500 text-xl font-bold my-2'>{price}$</p>
+      </div>
+    </div>
+  )
+}
+
+export default FoodItem
